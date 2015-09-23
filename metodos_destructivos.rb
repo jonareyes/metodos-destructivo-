e9a30230d9b2a1e74acd5a626380c51bc2893dd0
codeamac02 <codeamac02@gmail.com>
2015-09-23 16:38:15 -0500

    se prueba ejercicio con tabla de verdad para saber si el ejercicio funciona correctamente

--- metodos_destructivos.rb
+++ metodos_destructivos.rb
@@ -6,13 +6,18 @@ end
 def replace_spaces!(string)
 	string.swapcase!
 end
+
+
 str = "HOLa a toDo EL munDo"
+str_org = str.dup
 p "esto vale str: #{str}"
 p replace_spaces(str)
+p str == str_org
 p "esto vale str: #{str}"
 
 
 p replace_spaces!(str)
 p "esto vale str: #{str}"
+p str == str_org
 
 
